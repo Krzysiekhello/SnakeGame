195c6bed7b9aeb8017d04f818d4915d5486bbdbd
Krzysiek Makówka <krzysiekmakowka9@gmail.com>
2021-05-31 19:25:00 +0200

    snake logic done

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const gameField = document.querySelector("[data-gameField='gameField']")
-    let squareIndex = 0
-    let randomIndexElementWhichGonnaBeApple = Math.floor(Math.random() * 315)
-    const randomIndexElementWhereSnakeIsGoingToAppear = Math.floor(Math.random() * 315)
-    if (randomIndexElementWhichGonnaBeApple === randomIndexElementWhereSnakeIsGoingToAppear) {
-        randomIndexElementWhichGonnaBeApple = randomIndexElementWhichGonnaBeApple - 1
-    }
-    // Loop which add div elements into gameField element
-
-    for (let i = 0; i < 315 ; i++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.setAttribute("id", `${squareIndex}`);
-        if (i === randomIndexElementWhichGonnaBeApple) {
-            square.classList.add("apple");
+import {drawSnake, updateGame,selfSnakeBite,handleSnakeHead} from "./snakeFunctions.mjs";
+import {appleUpdate, updateAppleCoords} from "./appleFunctions.mjs";
+import {outsideGrid} from "./gridPostion.mjs";
+export const gameField = document.querySelector("[data-gameField='gameField']");
+let lastRenderTime = 0;
+let gameOver = false;
+const main = (currentTime) => {
+    if (gameOver) {
+        if (confirm("Lose, press Ok for next round")) {
+            window.location.reload();
         }
-        if (i === randomIndexElementWhereSnakeIsGoingToAppear) {
-            square.classList.add("snake");
-        }
-        gameField.appendChild(square);
-        squareIndex += 1
     }
-});
+    window.requestAnimationFrame(main);
+    const secsFromLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secsFromLastRender <  0.1) return;
+    lastRenderTime = currentTime;
+    draw();
+    update();
+}
+window.requestAnimationFrame(main);
+const snakeDeath = () => {
+     gameOver = outsideGrid(handleSnakeHead()) || selfSnakeBite();
+}
+const draw = () => {
+    gameField.innerHTML = '';
+    drawSnake(gameField);
+    appleUpdate(gameField);
+}
+const update = () => {
+    updateGame();
+    updateAppleCoords();
+    snakeDeath();
+}
